20fc149f748cecbc66a168f062e8851f68a017b9
Sahil Ghadi <sahilghadi514@gmail.com>
2026-07-20 20:32:59 +0530

    llm provider fix

--- lib/llm.ts
+++ lib/llm.ts
@@ -2,15 +2,15 @@ import { ChatOllama } from "@langchain/ollama";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 
 // Toggle this variable to easily switch between "ollama" and "gemini"
-const MODEL_PROVIDER: "ollama" | "gemini" = "gemini";
+//const MODEL_PROVIDER: "ollama" | "gemini" = "gemini";
 
 export const getModel = () => {
-  if (MODEL_PROVIDER === "ollama") {
-    return new ChatOllama({
-      model: "qwen2.5:3b",
-      temperature: 0.1,
-    });
-  }
+  // if (MODEL_PROVIDER === "ollama") {
+  //   return new ChatOllama({
+  //     model: "qwen2.5:3b",
+  //     temperature: 0.1,
+  //   });
+  // }
 
   // Default to Gemini
   return new ChatGoogleGenerativeAI({
